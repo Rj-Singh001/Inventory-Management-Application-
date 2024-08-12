27aff32e98ee3724f9ae4664e161b3382eea343a
Rj-Singh001 <153767117+Rj-Singh001@users.noreply.github.com>
2024-08-12 16:30:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+// function deleteProduct(id) {
+//     console.log("Deleting product with ID:", id);
+//     const result = confirm("Are you sure?");
+//     if (result) {
+//         fetch('/delete-product/' + id, {
+//             method: 'DELETE'
+//         }).then((res) => {
+//             if (res.ok) {
+//                 location.reload();
+//             } else {
+//                 console.error('Failed to delete product:', res.status, res.statusText);
+//             }
+//         }).catch((error) => {
+//             console.error('Error:', error);
+//         });
+//     }
+// }
+
+
+
+document.body.style.backgroundColor = "red";
